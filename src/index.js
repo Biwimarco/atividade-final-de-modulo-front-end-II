899a8a6577b849810879e3ddb6b88350243fbc92
Marco Antônio Wille <marcowille331@gmail.com>
2023-09-03 21:38:20 -0300

    Add func trocar img fundo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const arrImgs = ['background1.png', 'background2.jpeg', 'background3.jpg'];
+let index = 0;
+
+function switchImg() {
+    const body = document.body;
+    body.style.backgroundImage = `url(../images/${arrImgs[index]})`;
+    index = (index + 1) % arrImgs.length;
+}
+
+const iconSwitch = document.getElementById('iconHeader');
+iconSwitch.addEventListener('click', switchImg);
