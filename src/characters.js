1505a42ad86b9971f55b35c92b2f4f735a9befb8
Marco Antônio Wille <marcowille331@gmail.com>
2023-09-08 22:07:41 -0300

    Inseridos personagens no html

--- src/characters.js
+++ src/characters.js
@@ -1,11 +1,31 @@
-const api = (value) => {
-  const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
-  return result;
-};
+function getCharacters() {
+    try {
+    fetch('https://rickandmortyapi.com/api/character', {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(function(json){
 
-api(1);
+        let containerCards = document.querySelector('.container-cards');
+
+        json.results.map(function(results){
+            containerCards.innerHTML += `
+            <img src="${results.image}" alt="${results.name}">
+            <h2>${results.name}</h2>
+            <p>Status: ${results.status}</p>
+            <p>Species: ${results.species}</p>
+            <p>Type: ${results.type}</p>
+            <p>Gender: ${results.gender}</p>
+            <p>Origin: ${results.origin.name}</p>
+            <p>Location: ${results.location.name}</p>
+            <p>Episodes: ${results.episode.length}</p>
+            <p>Created: ${results.created}</p>
+            `
+        })
+    })
+    } catch (error) {
+        
+    }
+}
+
+getCharacters()
