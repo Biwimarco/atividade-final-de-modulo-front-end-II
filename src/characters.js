b3a80fd1969356288cda04e0bf264fdfbb4f31f4
Marco Antônio Wille <marcowille331@gmail.com>
2023-09-10 15:25:52 -0300

    Add func apliar card

--- src/characters.js
+++ src/characters.js
@@ -21,6 +21,11 @@ function getCharacters() {
               <p><strong>Location:</strong> <span>${results.location.name}</span></p>
               <p><strong>Episodes:</strong> <span>${results.episode.length}</span></p>
             `;
+
+          card.addEventListener("click", function () {
+            openModal(results);
+          });
+
           cardsContainer.appendChild(card);
         });
       });
@@ -28,3 +33,33 @@ function getCharacters() {
 }
 
 getCharacters();
+
+function openModal(character) {
+  const modal = document.getElementById("modal");
+  const modalContent = document.querySelector(".modal-content");
+
+  modalContent.innerHTML = `
+    <img src="${character.image}" alt="${character.name}">
+    <h2>${character.name}</h2>
+    <p><strong>Status:</strong> <span>${character.status}</span></p>
+    <p><strong>Species:</strong> <span>${character.species}</span></p>
+    <p><strong>Gender:</strong> <span>${character.gender}</span></p>
+    <p><strong>Origin:</strong> <span>${character.origin.name}</span></p>
+    <p><strong>Location:</strong> <span>${character.location.name}</span></p>
+    <p><strong>Episodes:</strong> <span>${character.episode.length}</span></p>
+  `;
+
+  modal.style.display = "block";
+  modalContent.style.background =
+    "linear-gradient(0deg, rgba(39,51,40,1) 47%, rgba(25,131,59,1) 100%)";
+  modalContent.style.textAlign = "center";
+
+  modal.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+
+  modal.removeEventListener("click", closeModal);
+}
